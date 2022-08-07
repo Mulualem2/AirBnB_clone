0af8d121a1f59e653b59ce207505a2d30152d6e7
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-08-07 22:50:46 +0300

    Update test_for_user.py

--- tests/test_models/test_for_user.py
+++ tests/test_models/test_for_user.py
@@ -1,27 +1,27 @@
+#!/usr/bin/python3
+"""Test suite for the User class in models.user"""
 import unittest
-from datetime import datetime
-from models import *
+from models.base_model import BaseModel
+
+from models.user import User
 
 
-class Test_UserModel(unittest.TestCase):
-    """
-    Test the user model class
-    """
+class TestUser(unittest.TestCase):
+    """Test cases against the User class"""
 
-    def setUp(self):
-        self.model = User()
-        self.model.save()
+    def test_attrs_are_class_attrs(self):
+        u = User()
+        # test that it is a class attribute
+        self.assertTrue(hasattr(User, "first_name")
+                        and hasattr(User, "last_name"))
 
-    def test_var_initialization(self):
-        self.assertTrue(hasattr(self.model, "email"))
-        self.assertTrue(hasattr(self.model, "password"))
-        self.assertTrue(hasattr(self.model, "first_name"))
-        self.assertTrue(hasattr(self.model, "last_name"))
-        self.assertEqual(self.model.email, "")
-        self.assertEqual(self.model.password, "")
-        self.assertEqual(self.model.first_name, "")
-        self.assertEqual(self.model.last_name, "")
+    def test_class_attrs(self):
+        u = User()
+        self.assertIs(type(u.first_name), str)
+        self.assertIs(type(u.last_name), str)
+        self.assertTrue(u.first_name == "")
+        self.assertTrue(u.last_name == "")
 
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_user_is_a_subclass_of_basemodel(self):
+        u = User()
+        self.assertTrue(issubclass(type(u), BaseModel))
