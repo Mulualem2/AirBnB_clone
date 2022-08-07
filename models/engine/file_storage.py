f28cced5d07e7c240090f3ed91ab6e05c32924ee
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-08-07 23:23:31 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,13 @@ Contains the FileStorage class model
 """
 import json
 
-from models import *
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
