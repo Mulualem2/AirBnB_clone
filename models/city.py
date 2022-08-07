079f307af1c9a8f5fdbe0a55b28834ee53bdff6d
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-08-07 23:05:40 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -2,11 +2,11 @@
 """Contains the City model"""
 from models.base_model import BaseModel
 
-
 class City(BaseModel):
     """Implements the City class"""
     state_id = ""
     name = ""
 
+
     def __init__(self, *args, **kwargs):
-	super().__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
