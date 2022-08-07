8d4345760a3faafcfa1f182b14bb00a77b9c0e49
Mulualem Mekonnen <88831286+Mulualem2@users.noreply.github.com>
2022-08-07 22:49:55 +0300

    Update test_for_state.py

--- tests/test_models/test_for_state.py
+++ tests/test_models/test_for_state.py
@@ -1,21 +1,23 @@
+#!/usr/bin/python3
+"""Test suite for the State class of the models.state module"""
 import unittest
-from datetime import datetime
-from models import *
+
+from models.base_model import BaseModel
+from models.state import State
 
 
-class Test_StateModel(unittest.TestCase):
-    """
-    Test the state model class
-    """
+class TestState(unittest.TestCase):
+    """Test cases for the State class"""
 
     def setUp(self):
-        self.model = State()
-        self.model.save()
+        self.state = State()
 
-    def test_var_initialization(self):
-        self.assertTrue(hasattr(self.model, "name"))
-        self.assertEqual(self.model.name, "")
+    def test_state_is_a_subclass_of_basemodel(self):
+        self.assertTrue(issubclass(type(self.state), BaseModel))
 
+    def test_attr_is_a_class_attr(self):
+        self.assertTrue(hasattr(self.state, "name"))
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_class_attrs(self):
+        self.assertIs(type(self.state.name), str)
+        self.assertFalse(bool(self.state.name))
